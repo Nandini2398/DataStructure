b2a58c12608dcb4f81df7f5d347cdc160c6d000c
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-25 16:02:54 +0530

    UPDATED:to insert the given value in between of node

--- LinkedlistProblem.java
+++ LinkedlistProblem.java
@@ -12,7 +12,29 @@ package com.bridgelabz.linkedlist;
 	        myLinkedList.insert(myFirstNode,mySecondNode);
 	        myLinkedList.search(mySecondNode);
 	        myLinkedList.printMyNodes();
-
+	        public void insertUsingKey(Node myNode,Node value){
+	            Node tempNode = this.head;
+	            while(tempNode != null ) {
+	                if (myNode.getKey() == tempNode.getKey()) {
+	                    Node temp =tempNode.getNext();
+	                    tempNode.setNext(value);
+	                    tempNode = tempNode.getNext();
+	                    tempNode.setNext(temp);
+	                    break;
+	                }
+	                tempNode=tempNode.getNext();
+	            }
+	        }
+	     public  void printMyNodes(){
+		        StringBuffer myNodes = new StringBuffer("My Nodes:");
+		        Node tempNode = head;
+		        while(tempNode.getNext()!= null){
+		            myNodes.append(tempNode.getKey());
+		            if(!tempNode.equals(tail))
+		                myNodes.append("->");
+		                tempNode=tempNode.getNext();
+		        }
+	        myNodes.append(tempNode.getKey());
+	        System.out.println(myNodes);
 	    }
-
 	}
