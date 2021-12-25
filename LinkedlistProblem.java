e7349af433215985f87b1fe7e3240f8d94783ec0
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-25 16:10:15 +0530

    UPDATED:to delete last element in between of node

--- LinkedlistProblem.java
+++ LinkedlistProblem.java
@@ -5,36 +5,15 @@ package com.bridgelabz.linkedlist;
 	        MyNode<Integer> myFirstNode = new MyNode<>(56);
 	        MyNode<Integer> mySecondNode = new MyNode<>(30);
 	        MyNode<Integer> myThirdNode = new MyNode<>(70);
-	        //MyNode<Integer> myFourthNode = new MyNode<>(40);
+	        MyNode<Integer> myFourthNode = new MyNode<>(40);
 	        MyLinkedList myLinkedList = new MyLinkedList();
 	        myLinkedList.add(myFirstNode);
 	        myLinkedList.append(myThirdNode);
 	        myLinkedList.insert(myFirstNode,mySecondNode);
 	        myLinkedList.search(mySecondNode);
+	        myLinkedList.insertUsingKey(mySecondNode,myFourthNode);
+	        myLinkedList.deleteKey(myFourthNode);
 	        myLinkedList.printMyNodes();
-	        public void insertUsingKey(Node myNode,Node value){
-	            Node tempNode = this.head;
-	            while(tempNode != null ) {
-	                if (myNode.getKey() == tempNode.getKey()) {
-	                    Node temp =tempNode.getNext();
-	                    tempNode.setNext(value);
-	                    tempNode = tempNode.getNext();
-	                    tempNode.setNext(temp);
-	                    break;
-	                }
-	                tempNode=tempNode.getNext();
-	            }
-	        }
-	     public  void printMyNodes(){
-		        StringBuffer myNodes = new StringBuffer("My Nodes:");
-		        Node tempNode = head;
-		        while(tempNode.getNext()!= null){
-		            myNodes.append(tempNode.getKey());
-		            if(!tempNode.equals(tail))
-		                myNodes.append("->");
-		                tempNode=tempNode.getNext();
-		        }
-	        myNodes.append(tempNode.getKey());
-	        System.out.println(myNodes);
+	        myLinkedList.size();
 	    }
-	}
+}
