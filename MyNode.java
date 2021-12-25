12802c7cba8b33ad45aa9bd66a257f4c8c43cc98
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-25 16:50:33 +0530

    ADDED:creating of queue

--- MyNode.java
+++ MyNode.java
@@ -1,25 +1,25 @@
 package com.bridgelabz.queues;
 
-	public class MyNode<K extends Comparable> implements INode<K> {
-        private K key;
-        private INode next;
+public class MyNode<K extends Comparable> implements Node<K> {
+    private K key;
+    private Node next;
 
-        public MyNode(K key) {
-            this.key =key;
-            this.next=null;
-        }
-        @Override
-        public K getKey() {
-            return key;
-        }
-        @Override
-        public void setKey() {
-            this.key=key;
-        }
-        public INode<K> getNext() {
-            return next;
-        }
-        public void setNext(INode next) {
-            this.next =(MyNode<K>) next;
-        }
+    public MyNode(K key) {
+        this.key =key;
+        this.next=null;
+    }
+    @Override
+    public K getKey() {
+        return key;
+    }
+    @Override
+    public void setKey() {
+        this.key=key;
+    }
+    public Node<K> getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next =(MyNode<K>) next;
+    }
 }
